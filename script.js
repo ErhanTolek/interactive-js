6601d9227de3fa7f4c1aef8f7a1db854bce7376e
erhantolek <erhantolek2207@gmail.com>
2022-11-14 10:39:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const maincard = document.querySelector(".main-container");
+const main = document.getElementById("main");
+const ratebuttonslist = document.querySelector("ul");
+const submitbtn = document.querySelector(".btn-submit");
+const thnkcard = document.getElementById("thank-card");
+const ratevalue = document.querySelector(".banner");
+const container = document.getElementById("container");
+let rate;
+
+ratebuttonslist.addEventListener("click", rating);
+submitbtn.addEventListener("click", submiting);
+
+thnkcard.style.display = "none";
+
+function rating(e) {
+  if (e.target.className === "btn") {
+    localStorage.setItem("rate", e.target.innerText);
+  }
+}
+
+function submiting(e) {
+  maincard.remove();
+  ratevalue.innerText = `You selected ${getRatefromLS()} out of 5 `;
+  thnkcard.style.display = "initial";
+}
+
+function getRatefromLS() {
+  const value1 = parseInt(localStorage.getItem("rate"));
+
+  return value1;
+}
+
+getRatefromLS();
